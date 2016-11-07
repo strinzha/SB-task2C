cfbb38c0e9b489faa3bf29d77d74890ae10a0f33
Boris Strinzha <bs@clickky.biz>
2016-11-07 22:23:31 +0200

    a plu b task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-function hello(name) {
-  console.log(`Hello ${name}`); // eslint-disable-line
-}
-hello('JS World');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const a = Number(req.query.a) || 0;
+  const b = Number(req.query.b) || 0;
+  const summ = a + b;
+  res.send(summ.toString());
+});
+
+app.listen(3000, () => {});
