8c685b8a8564f7b7dd5358920e5b977931307cac
Boris Strinzha <bs@clickky.biz>
2016-11-17 23:03:22 +0200

    task 2C

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  const a = Number(req.query.a) || 0;
-  const b = Number(req.query.b) || 0;
-  const summ = a + b;
-  res.send(summ.toString());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.get('/task2C', (req, res) => {
+  const re = /^@?(https:|http:)?(\/\/)?(www.)?(vk.com\/|twitter.com\/|telegram.me\/|github.com\/)?(\w+)/;
+  const username = req.query.username;
+  const matches = username.match(re);
+  res.send(`@${matches[matches.length - 1]}`);
 });
 
 app.listen(3000, () => {});
